64b1fd9bfdcf38ab07dc6bcfebb6fc25c5a52f44
LastInstance <123825454+LastInstance@users.noreply.github.com>
2023-02-22 17:24:59 +0200

    Update basicclasses2.py

--- basicclasses2.py
+++ basicclasses2.py
@@ -3,69 +3,41 @@ class Vehicle():
         self.cost = cost
         self.colour = colour
         self.mark = mark
-        
+
     def __str__(self):
         return f'Your choice is {self.mark} and the colour is {self.colour}\nIt cost {self.cost} '
-
 
     def __repr__(self):
         return f'Vehicle(\'{self.mark}\', {self.colour}, {self.cost}'
 
 
 class Moto(Vehicle):
-    def speed(self, speed):
+
+    def __init__(self, cost, colour, mark, speed):
+        super().__init__(cost, colour, mark)
         self.speed = speed
-        print("Maximum speed of this moto is" , self.speed)
-    def cost(self):
-        super().cost()
-        return f'Cost of this moto is {self.cost}'
-    def mark(self):
-        super().mark()
-        return f'This is {self.mark}'
-    def colour(self):
-        super().colour()
-        return f'His colour is {self.colour}'
+        print("Maximum speed of this moto is", self.speed)
+
 
 class Auto(Vehicle):
-    def type(self, type):
+
+
+    def __init__(self, cost, colour, mark, type, seats):
+        super().__init__(cost, colour, mark)
         self.type = type
-        print("Type of this auto is", self.type)
-
-    def seats(self, seats):
         self.seats = seats
-        print ("Number of seats of this auto is", self.seats)
+        print("Type of this car is", self.type)
+        print("The number of seats is", self.seats)
 
-    def mark(self, mark):
-        super().mark()
-        return f'This is {self.mark}'
-    def cost(self, cost):
-        super().cost()
-        return f'It costs {self.cost}'
-    def colour(self, colour):
-        super().colour()
-        return f'Colour of this auto is {self.colour}'
 
 class Boat(Vehicle):
 
-    def speed(self, speed):
+    def __init__(self, cost, colour, mark, speed, type ):
+        super().__init__(cost, colour, mark)
         self.speed = speed
-        print('Maximum speed of this boat is', self.speed)
-
-    def type(self, type):
         self.type = type
-        return ('Type of this boat is', self.type)
-
-    def cost(self, cost):
-        super().cost()
-        return f' Cost of this boat is {self.cost}'
-
-    def colour(self, colour):
-        super().colour()
-        return f'Colour of this boat is {self.colour}'
-
-    def mark(self, mark):
-        super().mark()
-        return f'Mark of this boat is {self.mark}'
+        print("Maximum speed of this boat is", self.speed)
+        print("Type of this boat is", self.type)
 
 
 
